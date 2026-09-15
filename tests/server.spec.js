fc5ad1d11fa0b477f2491f8cc3dbeae517d1d0b4
Matthew <mccolvig@gmail.com>
2026-09-15 17:35:11 -0600

    Deliver it well v2

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says my girlfriend is beautiful', (done) => {
+  chai.request(server)
+    .get('/')
+    .end((err, res) => {
+      expect(err).not.exist;
+      expect(JSON.stringify(res.text)).to.contain('My girlfriend is BEAUTIFUL');
+      done();
+    });
+  });
 })
